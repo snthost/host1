a375d52ba394d454a03cde191856a70614325cf5
snthost <35021008+snthost@users.noreply.github.com>
2018-01-02 15:58:51 -0600

    Update val33a.js

--- val33a.js
+++ val33a.js
@@ -42,3 +42,10 @@ class CampoNumerico {
 }
 
 new CampoNumerico('#atributoaletras');
+simpleCart.bind('beforeAdd', function (newitem) {
+simpleCart.each(function (cartitem) {
+    if (cartitem.get("name") === newitem.get("name")) {
+        cartitem.remove();
+    }
+})
+});
