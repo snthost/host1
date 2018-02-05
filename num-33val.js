d5733b528e06796aec389c54cdfb85a5945e4e08
snthost <35021008+snthost@users.noreply.github.com>
2018-02-05 11:26:46 -0600

    Update num-33val.js

--- num-33val.js
+++ num-33val.js
@@ -1,2 +1,2 @@
 $("input#atributoaletras1").keypress(function(t){
-  return t.which<48||t.which>57||2===this.value.length?(atributoaletras1.value=""):void 0});
+  return t.which<46||t.which>57||2===this.value.length?(atributoaletras1.value=""):void 0});
