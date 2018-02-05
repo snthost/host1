ed5123f70a5a6d2615f75e9d92e35ff07006541c
snthost <35021008+snthost@users.noreply.github.com>
2018-02-05 11:19:16 -0600

    Update num-33val.js

--- num-33val.js
+++ num-33val.js
@@ -4,7 +4,7 @@
       function isNumberKey(evt)
       {
          var charCode = (evt.which) ? evt.which : event.keyCode
-         if (charCode > 31 && (charCode < 48 || charCode > 57 ||2===this.value.length?(atributoaletras1.value="",!1)))
+         if (charCode > 31 && (charCode < 48 || charCode > 57))
             return false;
  
          return true;
