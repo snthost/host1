8474bb63abbe6f0048b60cf93999b44fb1f5eb0a
snthost <35021008+snthost@users.noreply.github.com>
2018-02-05 11:23:48 -0600

    Update num-33val.js

--- num-33val.js
+++ num-33val.js
@@ -1,11 +1,2 @@
-/*$("input#atributoaletras1").keypress(function(t){
-  return t.which<48||t.which>57||2===this.value.length?(atributoaletras1.value="",!1):void 0});
-*/
-      function isNumberKey(evt)
-      {
-         var charCode = (evt.which) ? evt.which : event.keyCode
-         if (charCode > 31 && (charCode < 48 || charCode > 57))
-            return false;
- 
-         return true;
-      }
+$("input#atributoaletras1").keypress(function(t){
+  return t.which<48||t.which>57||3===this.value.length?(atributoaletras1.value="",!1):void 0});
