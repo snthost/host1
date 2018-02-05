2206d7ceb1171aacdb282b9e3796410f74135dec
snthost <35021008+snthost@users.noreply.github.com>
2018-02-05 11:15:49 -0600

    Update num-33val.js

--- num-33val.js
+++ num-33val.js
@@ -1,3 +1,11 @@
 /*$("input#atributoaletras1").keypress(function(t){
   return t.which ===0||t.which<48||t.which>57||2===this.value.length?(atributoaletras1.value="",!1):void 0});
 */
+      function isNumberKey(evt)
+      {
+         var charCode = (evt.which) ? evt.which : event.keyCode
+         if (charCode > 31 && (charCode < 48 || charCode > 57))
+            return false;
+ 
+         return true;
+      }
