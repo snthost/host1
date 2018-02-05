8e921802711f512495f95ac78e89f7348198a249
snthost <35021008+snthost@users.noreply.github.com>
2018-02-05 10:54:49 -0600

    Update num-33val.js

--- num-33val.js
+++ num-33val.js
@@ -1,1 +1,18 @@
-$("input#atributoaletras1").keypress(function(t){return t.which<48||t.which>57||2===this.value.length?(atributoaletras1.value="",!1):void 0});
+$("input#atributoaletras1").keypress(function(t){
+      tecla = (document.all) ? e.keyCode : e.which;
+    if (tecla==8) return true; //Tecla de retroceso (para poder borrar)
+    if (tecla==44) return true; //Coma ( En este caso para diferenciar los decimales )
+    if (tecla==48) return true;
+    if (tecla==49) return true;
+    if (tecla==50) return true;
+    if (tecla==51) return true;
+    if (tecla==52) return true;
+    if (tecla==53) return true;
+    if (tecla==54) return true;
+    if (tecla==55) return true;
+    if (tecla==56) return true;
+    patron = /1/; //ver nota
+    te = String.fromCharCode(tecla);
+    return patron.test(te);
+} 
+  return t.which<48||t.which>57||2===this.value.length?(atributoaletras1.value="",!1):void 0});
