a471fc81c51a9002784eb76ff9d67447077982aa
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-07-17 11:27:50 -0600

    Update resistencia-min.js

--- media/resistencia-min.js
+++ media/resistencia-min.js
@@ -20,7 +20,7 @@ colorBandArray[0]=[0,1,2,3,4,5,6,7,8,9];
 colorBandArray[1]=[0,1,2,3,4,5,6,7,8,9];
 colorBandArray[2]=[0,1,2,3,4,5,6,7,8,9];
 colorBandArray[3]=[0,1,2,3,4,5,6,7,10,11];
-colorBandArray[4]=[1,10,11];
+colorBandArray[4]=[1];
 
 namesArray[1]=["&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;","&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"];
 
