996aa92032bec35239e837680e02dea205fd12d5
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-07-16 21:09:25 -0600

    Update resistencia-min.js

--- media/resistencia-min.js
+++ media/resistencia-min.js
@@ -22,7 +22,7 @@ colorBandArray[2]=[0,1,2,3,4,5,6,7,8,9];
 colorBandArray[3]=[0,1,2,3,4,5,6,7,10,11];
 colorBandArray[4]=[1,10,11];
 
-namesArray[1]=["&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"];
+namesArray[1]=["&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"];
 
 var vuoto = cart + "band_blank" + est;
 var cart_pref = cart + "band_";
