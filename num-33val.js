46a6c259c6117651829af6ff61940a22a6f99daf
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-07-26 21:36:36 -0600

    Update num-33val.js

--- num-33val.js
+++ num-33val.js
@@ -1,5 +1,5 @@
-$("input#atributoaletras1").keypress(function(t){
-  return t.which<46||t.which>57||2===this.value.length?(atributoaletras1.value="",!1):void 0});
+/*$("input#atributoaletras1").keypress(function(t){
+  return t.which<46||t.which>57||2===this.value.length?(atributoaletras1.value="",!1):void 0});*/
 
 $('input#atributoaletras1')
   .keypress(function (event) {
