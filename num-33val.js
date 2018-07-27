268547434b6dad9b6dd1d8c726d52930de516625
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-07-26 21:44:58 -0600

    Update num-33val.js

--- num-33val.js
+++ num-33val.js
@@ -1,5 +1,5 @@
 /*$("input#atributoaletras1").keypress(function(t){
-  return t.which<46||t.which>57||2===this.value.length?(atributoaletras1.value="",!1):void 0});*/
+  return t.which<48||t.which>57||2===this.value.length?(atributoaletras1.value="",!1):void 0});*/
 
 $(document).ready(function () {
   $('input#atributoaletras1')
