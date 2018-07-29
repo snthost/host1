4cf3cec3eb53fee85a1145f5d51d0dc876656ef9
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-07-29 02:31:21 -0600

    Update val33a.js

--- val33a.js
+++ val33a.js
@@ -45,6 +45,6 @@ simpleCart.bind('beforeAdd', function (newitem) {
 simpleCart.each(function (cartitem) {
     if (cartitem.get("name") === newitem.get("name")) {
         cartitem.remove();
-    }
+   }
 })
 });
