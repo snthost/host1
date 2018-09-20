bce886289397ddcac264e7647752a76af1397300
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-09-19 18:19:17 -0600

    Update bloqueo-CyP.js

--- bloqueo-CyP.js
+++ bloqueo-CyP.js
@@ -1,6 +1,8 @@
 /*eSNT-host-bloque-de-emails-mailchimps*/
 window.onload = function() {
   var myInput = document.getElementById('fname');
+  var myInput = document.getElementById('lname');
+  var myInput = document.getElementById('email');
   myInput.onpaste = function(e) {
     e.preventDefault();
     alert("esta acción está prohibida");
