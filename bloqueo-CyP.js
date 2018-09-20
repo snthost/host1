2406153a31d4fa4ef11bbca5c6fdd7c36f830cde
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-09-19 18:26:25 -0600

    Update bloqueo-CyP.js

--- bloqueo-CyP.js
+++ bloqueo-CyP.js
@@ -13,3 +13,27 @@ window.onload = function() {
     alert("no puedes copiar este dato");
   }
 }
+window.onload = function() {
+  var myInput = document.getElementById('lname');
+  myInput.onpaste = function(e) {
+    e.preventDefault();
+    alert("esta acción está prohibida");
+  }
+  
+  myInput.oncopy = function(e) {
+    e.preventDefault();
+    alert("no puedes copiar este dato");
+  }
+}
+window.onload = function() {
+  var myInput = document.getElementById('email');
+  myInput.onpaste = function(e) {
+    e.preventDefault();
+    alert("esta acción está prohibida");
+  }
+  
+  myInput.oncopy = function(e) {
+    e.preventDefault();
+    alert("no puedes copiar este dato");
+  }
+}
