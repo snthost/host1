32654ee5d795c45d9e34cf5cf30a357be5b63121
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-09-19 18:30:22 -0600

    Update bloqueo-CyP.js

--- bloqueo-CyP.js
+++ bloqueo-CyP.js
@@ -1,37 +1,14 @@
 /*eSNT-host-bloque-de-emails-mailchimps*/
 window.onload = function() {
-  var myInput = document.getElementById('fname');
+var myInput = document.getElementById('fname');
+var myInput = document.getElementById('lname');
+var myInput = document.getElementById('email');
   myInput.onpaste = function(e) {
     e.preventDefault();
     alert("esta acción está prohibida");
-  }
-  
-  myInput.oncopy = function(e) {
+                                }
+  myInput.oncopy = function(e)  {
     e.preventDefault();
     alert("no puedes copiar este dato");
-  }
-}
-window.onload = function() {
-  var myInput = document.getElementById('lname');
-  myInput.onpaste = function(e) {
-    e.preventDefault();
-    alert("esta acción está prohibida");
-  }
-  
-  myInput.oncopy = function(e) {
-    e.preventDefault();
-    alert("no puedes copiar este dato");
-  }
-}
-window.onload = function() {
-  var myInput = document.getElementById('email');
-  myInput.onpaste = function(e) {
-    e.preventDefault();
-    alert("esta acción está prohibida");
-  }
-  
-  myInput.oncopy = function(e) {
-    e.preventDefault();
-    alert("no puedes copiar este dato");
-  }
-}
+                                }
+                          }
