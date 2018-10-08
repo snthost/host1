9aea4cbca17f842cd8b0f7334f3cf659f0d7472b
electronicasnt.site <35021008+snthost@users.noreply.github.com>
2018-10-08 14:38:40 -0600

    Update mailchimp.js

--- nueva-plantilla/js/mailchimp.js
+++ nueva-plantilla/js/mailchimp.js
@@ -1,1 +1,46 @@
-!function(){window.$mcSite||($mcSite={},$mcSite.popup_form={settings:{base_url:"mc.us17.list-manage.com",list_id:"fa9318b42e",uuid:"bb78271ff493b3d5ad92cac6e"}})}(),function(){if(void 0!==window.$mcSite&&void 0!==window.$mcSite.popup_form&&!(window.location.href.indexOf("checkout.shopify")>=0)){for(var i=document.getElementsByTagName("script"),o=0;o<i.length;o++)if("//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js"===i[o].getAttribute("src")||"//downloads.mailchimp.com/js/signup-forms/popup/embed.js"===i[o].getAttribute("src"))return;var e=window.$mcSite.popup_form;if(e.installed!==!0&&e.settings){var t=e.settings;if(t.base_url&&t.uuid&&t.list_id){var s=document.createElement("script");s.src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js",s.type="text/javascript",s.onload=function(){require(["mojo/signup-forms/Loader"],function(i){i.start({baseUrl:t.base_url,uuid:t.uuid,lid:t.list_id})})},document.body.appendChild(s),window.$mcSite.popup_form.installed=!0}}}}();
+(function(){if(!window.$mcSite){$mcSite={};$mcSite.popup_form={settings:{base_url:"mc.us17.list-manage.com",list_id:"fa9318b42e",uuid:"bb78271ff493b3d5ad92cac6e"}};}})();
+(function () {
+    if (window.$mcSite === undefined || window.$mcSite.popup_form === undefined) {
+        return;
+    }
+
+    if (window.location.href.indexOf("checkout.shopify") >= 0) {
+        return;
+    }
+
+    var scripts = document.getElementsByTagName("script");
+    for (var i = 0; i < scripts.length; i++) {
+        // look for src containing the old embed.js code and bail if it exists
+        if (scripts[i].getAttribute("src") === "https://s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" || scripts[i].getAttribute("src") === "//downloads.mailchimp.com/js/signup-forms/popup/embed.js") {
+            return;
+        }
+    }
+
+    var module = window.$mcSite.popup_form;
+
+    if (module.installed === true) {
+        return;
+    }
+
+    if (!module.settings) {
+        return;
+    }
+
+    var settings = module.settings;
+
+    if (!settings.base_url || !settings.uuid || !settings.list_id) {
+        return;
+    }
+
+    var script = document.createElement("script");
+
+    script.src = "https://downloads.mailchimp.com/js/signup-forms/popup/embed.js";
+    script.type = "text/javascript";
+    script.onload = function () {
+        require(["mojo/signup-forms/Loader"], function (L) { L.start({"baseUrl": settings.base_url, "uuid": settings.uuid, "lid": settings.list_id}); });
+    };
+
+    document.body.appendChild(script);
+
+    window.$mcSite.popup_form.installed = true;
+}());
